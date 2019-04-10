08f00ff65ca1396cd91f969886af6cf121f738db
Mauricio Aldazosa Mariaca <mauricio.aldazosa@ciencias.unam.mx>
2019-04-09 19:32:35 -0500

    Commit inicial.

---
+++ tst.js
@@ -0,0 +1,24 @@
+import { Manager, Reference, Popper } from 'react-popper';
+
+const Example = () => (
+  <Manager>
+    <Reference>
+      {({ ref }) => (
+        <button type="button" ref={ref}>
+          Reference element
+        </button>
+      )}
+    </Reference>
+    <Popper placement="right">
+      {({ ref, style, placement, arrowProps }) => (
+        <div ref={ref} style={style} data-placement={placement}>
+          Popper element
+          <div ref={arrowProps.ref} style={arrowProps.style} />
+        </div>
+      )}
+    </Popper>
+  </Manager>
+);
+
+const rootNode = document.querySelector('#root');
+rootNode && ReactDOM.render(<Example />, rootNode);
